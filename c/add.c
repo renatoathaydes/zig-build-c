d2e41cc6dfdfe3cd3aea4c5bfeb11eeeac28dc6a
Renato Athaydes <renato@athaydes.com>
2022-06-19 14:30:56 +0200

    Test C count_bytes function in Zig

--- c/add.c
+++ c/add.c
@@ -2,3 +2,10 @@ int add(int a, int b) {
   return a + b;
 }
 
+int count_bytes(char *str) {
+  int count = 0;
+  while(str[count]) {
+    count++;
+  }
+  return count;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,14 @@
-const adder = @cImport({
+const clib = @cImport({
     @cInclude("add.c");
 });
 const std = @import("std");
 const testing = std.testing;
 
 test "basic add functionality" {
-    try testing.expectEqual(@as(i32, 10), adder.add(3, 7));
+    try testing.expectEqual(@as(i32, 10), clib.add(3, 7));
+}
+
+test "counting bytes in C" {
+    var cstr = "ABC".*;
+    try testing.expectEqual(@as(i32, 3), clib.count_bytes(&cstr));
 }
