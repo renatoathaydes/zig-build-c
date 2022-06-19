0d48545faf15777a77cae11cbf538bbc0ed72abc
Renato Athaydes <renato@athaydes.com>
2022-06-19 14:17:58 +0200

    Use C add function instead of Zig

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,9 @@
+const adder = @cImport({
+    @cInclude("add.c");
+});
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
 test "basic add functionality" {
-    try testing.expectEqual(@as(i32, 10), add(3, 7));
+    try testing.expectEqual(@as(i32, 10), adder.add(3, 7));
 }
